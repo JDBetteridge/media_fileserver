02c3334e8aa77caf5e00d4112b76521eb30840fa
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2023-01-22 20:55:15 +0000

    Basic functionality without tarballing

--- mfs/media_fileserver.py
+++ mfs/media_fileserver.py
@@ -2,34 +2,76 @@ import os
 
 from flask import (Flask, request, render_template,
                     make_response, send_from_directory,
-                    redirect, url_for, session)
-from glob import glob
-
+                    redirect, url_for, session, escape)
+# ~ from glob import glob
+from pathlib import Path
+from urllib.parse import quote, unquote
+from werkzeug.utils import secure_filename
 
 host = "127.0.0.1"
 port = 8080
 mfs = Flask(__name__)
+directory = Path("/srv/torrents")
 
-def get_log():
-    path = os.path.join(os.getcwd(), "log", "*.csv")
-    return [Path(f) for f in reversed(sorted(glob(path)))]
 
-@mfs.route("/list")
-def list():
-    csvfiles = get_log()
-    return render_template("nav.html", csvfiles=csvfiles)
+class SafePathName:
+    def __init__(self, path):
+        if isinstance(path, Path):
+            self.path = path
+        else:
+            self.path = Path(path)
+        self.name = self.path.name
+        self.html = str(escape(self.name))
+        self.quote = quote(self.name)
+        self.filename = secure_filename(self.name)
 
-@mfs.route("/list/<name>")
-def download(name):
-    path = os.path.join(os.getcwd(), "log")
-    return send_from_directory(path, name, as_attachment=True)
 
-@mfs.route("/list/raw/<name>")
-def raw(name):
-    path = os.path.join(os.getcwd(), "log", name)
-    with open(path) as fh:
-        contents = [line for line in fh.readlines()]
-    return render_template("raw.html", raw=contents)
+def get_files(path):
+    return [SafePathName(f) for f in sorted(path.glob("*"))]
+
+
+# ~ @mfs.route("/")
+# ~ def list():
+    # ~ filelist = get_files(directory)
+    # ~ return render_template("nav.html", filelist=filelist)
+
+@mfs.route("/")
+@mfs.route("/<path:path>")
+def list2(path="."):
+    print(path)
+    filelist = get_files(directory/Path(path))
+    safepath = SafePathName(path)
+    parent = SafePathName(safepath.path.parent) if path != "." else None
+    return render_template("nav.html", path=safepath, parent=parent, filelist=filelist)
+
+
+@mfs.route("/download/<path:path>/<name>")
+def download(path, name):
+    return send_from_directory(directory/path, name, as_attachment=True)
+
+
+# ~ @mfs.route("/raw/<name>")
+# ~ def raw(name):
+    # ~ path = directory/name
+    # ~ print(path, flush=True)
+    # ~ with open(path) as fh:
+        # ~ contents = [line for line in fh.readlines()]
+    # ~ return render_template("raw.html", raw=contents)
+@mfs.route("/raw/<path:path>/<name>")
+def raw(path, name):
+    # ~ path = directory/name
+    # ~ with open(path) as fh:
+        # ~ contents = [line for line in fh.readlines()]
+    ext = Path(name).suffix
+    if ext:
+        ext = ext[1:]
+    if ext in ["mp4"]:
+        return send_from_directory(directory/path, name, as_attachment=False, mimetype="video/" + ext)
+    elif ext in ["txt", ""]:
+        with open(directory/path/name) as fh:
+            contents = [line for line in fh.readlines()]
+        return render_template("raw.html", raw=contents)
+
 
 def main():
     # This allows us to use a plain HTTP callback
